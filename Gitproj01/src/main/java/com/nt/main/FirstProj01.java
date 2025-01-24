a323451f905b3451b5af4eadddae9df7f77f3ee7
Prasanta Ku.Nayak <prasantan154@gmail.com>
2025-01-24 18:45:22 +0530

    z

--- Gitproj01/src/main/java/com/nt/main/FirstProj01.java
+++ Gitproj01/src/main/java/com/nt/main/FirstProj01.java
@@ -3,7 +3,11 @@ package com.nt.main;
 public class FirstProj01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 20;
+		
+		int z = x+y;
+		System.out.println(z);
 
 	}
 
